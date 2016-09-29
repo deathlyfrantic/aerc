9296d11c4842b2355c944f6fe9d2e5988364ccbd
Harry Jeffery <harry@exec64.co.uk>
2016-09-29 23:31:00 +0100

    stringop.c: add is_prefix_of()

--- src/util/stringop.c
+++ src/util/stringop.c
@@ -49,6 +49,14 @@ void strip_quotes(char *str) {
 	*end = '\0';
 }
 
+int is_prefix_of(const char* prefix, const char* str) {
+	while(*prefix) {
+		if(*prefix++ != *str++)
+			return 0;
+	}
+	return 1;
+}
+
 list_t *split_string(const char *str, const char *delims) {
 	/*
 	 * Splits up a string at each delimiter, and returns a list_t with the
